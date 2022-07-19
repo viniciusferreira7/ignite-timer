f2ba1bad3b934a6982f951a32333b7941c56febf
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-07-19 19:14:54 -0300

    style(global): colors & fonts

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -14,7 +14,12 @@ const buttonVariants = {
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-  width:200px;
-  height: 50px;
-  background:${props => props.theme.primary };
+  width:100px;
+  height: 40px;
+  border: 0;
+  border-radius: 4px;
+  margin: 8px;
+
+  background:${props => props.theme['green-500'] };
+  color:${props => props.theme.white };
 `
