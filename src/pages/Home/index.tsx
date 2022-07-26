6cf21a0490fadbfd854e846c6c33aa49bb90b27c
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-07-25 21:54:31 -0300

    style(home): html structure and styling

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,38 @@
+import { Play } from 'phosphor-react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input type="text" id="task" />
+
+          <label htmlFor="minutesAmount">Duração</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,6 +15,7 @@ export const GlobalStyle = createGlobalStyle`
   body{
     background-color: ${({ theme }) => theme['gray-900']};
     color: ${({ theme }) => theme.white};
+    -webkit-font-smoothing:antialiased;
   }
 
   body, input, textarea, button {
